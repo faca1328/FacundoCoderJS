fee7f1e66a21d2d7589641659bf168a734579fd0
Facu M <facundoivanmartinez@hotmail.com>
2022-07-17 21:16:43 -0300

    entrega 18/07

--- script.js
+++ script.js
@@ -1,3 +1,78 @@
+class libro {
+
+    constructor(titulo,autor,genero,precio){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.genero=genero;
+        this.precio=precio;
+    }
+}
+
+const libreria = [
+    new libro("la poesia de adolfo becquer", "adolfo becquer" , "desarrollo personal" , 800),
+    new libro("eragon", "christopher paolin", "fantasia", 2300),
+    new libro("cronicas de la torre 1", "laura gallego garcia", "fantasia", 1250),
+    new libro("el demonio de arbennios", "bernard torello", "fantasia", 1800),
+    new libro("fuego y sangre", "george rr martin", "fantasia", 4700),
+    new libro("mareas de medianoche", "steven erikson", "fantasia", 980),
+    new libro("habitos atomicos", "james clear", "desarrollo personal", 1300),
+    new libro("harry potter y el caliz de fuego", "jk rowling", "fantasia", 2100),
+    new libro("kings of the wilds", "nicholas eames", "fantasia", 1999),
+    new libro("el señor de los anillos 1", "jrr tolkien" , "fantasia", 2100),
+    new libro("padre rico padre pobre", "robert t kiyosaki", "desarrollo personal", 3000),
+    new libro("piense y hagase rico", "napoleon hill", "desarrollo personal", 2300)
+];
+
+let buscar = true;
+
+while (buscar){
+    let buscador = prompt("ingresa el genero que mas te guste (fantasia o desarrollo personal) para ver nuestro catalogo. \nPara salir ingresar: x");
+
+    if(buscador.toLowerCase()== "x"){
+        buscar=false;
+        break;}
+
+    switch(buscador.toLowerCase()){
+        case "fantasia" :
+            const filtrado=libreria.filter(elemento => elemento.genero == "fantasia");
+            alert(filtrado);
+
+
+/*             let filtrado = libreria.filter((libro)=>libro.genero.toLowerCase().includes(buscador.toLowerCase()));
+            function mostrar (array){
+                let info = " ";
+                array.foreach(element=>{
+                    info+= "Titulo: "+element.titulo+"\nAutor: "+element.autor+"\nPrecio: $"+element.precio+"\n\n"
+                })
+                return info;
+            }
+            alert(mostrar(filtrado(buscador,libreria))); */
+            break;
+
+        case "desarrollo personal" :
+            const filtrado2=libreria.filter(elemento => elemento.genero == "desarrollo personal");
+            alert(filtrado2);
+            break;
+
+        default:
+            alert("elegir entre el genero ¨fantasia¨ o ¨desarrollo personal¨ ");
+            break; 
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+/* Codigo de entregas anteriores 17/7/22 */
+
 /* 
 let nombreusuario = prompt("ingresa tu Nombre")
 
@@ -6,7 +81,7 @@ alert("Bienvenido/a " + nombreusuario)
 let password = false
 
 for (let i = 2; i >= 0; i--) {
-    let userpassword = prompt("busca tu password de 4 digitos :)");
+    let userpassword = prompt("busca tu password de 4 digitos (9080)");
     if (userpassword == 9080) {
         alert("Bien! sigamos")
         password = true
@@ -45,16 +120,6 @@ let caballoblanco;
             break; 
         }
     } while (caballoblanco != "blanco" );
- */
-
-/* 
-let palabraoculta = "aquiles"
-
-do{ palabraocultausuario= prompt("Ultima!, Busca la palabra Oculta !!");
-
-    if (palabraoculta = palabraocultausuario) { "¡ Aquiles Bailoyo !"}
-    else {alert("mira un poco mas arriba")};
-}while(palabraoculta != "aquiles" );
 
  */
 
@@ -65,31 +130,23 @@ do{ palabraocultausuario= prompt("Ultima!, Busca la palabra Oculta !!");
 const grande    = document.querySelector('.grande')
 const punto     = document.querySelectorAll('.punto')
 
-// Cuando CLICK en punto
-    // Saber la posición de ese punto
-    // Aplicar un transform translateX al grande
-    // QUITAR la clase activo de TODOS puntos
-    // AÑADIR la clase activo al punto que hemos hecho CLICK
-
-// Recorrer TODOS los punto
 punto.forEach( ( cadaPunto , i )=> {
-    // Asignamos un CLICK a cadaPunto
     punto[i].addEventListener('click',()=>{
 
-        // Guardar la posición de ese PUNTO
+        
         let posicion  = i
-        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        
         let operacion = posicion * -50
 
-        // MOVEMOS el grand
+        
         grande.style.transform = `translateX(${ operacion }%)`
 
-        // Recorremos TODOS los punto
+        
         punto.forEach( ( cadaPunto , i )=>{
-            // Quitamos la clase ACTIVO a TODOS los punto
+            
             punto[i].classList.remove('activo')
         })
-        // Añadir la clase activo en el punto que hemos hecho CLICK
+        
         punto[i].classList.add('activo')
 
     })
